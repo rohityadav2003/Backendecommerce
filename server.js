1f834114b3bd29074ef5519ca1c399e5aaaf16d6
rohityadav2003 <rohityadav20067@gmail.com>
2025-06-02 14:44:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
-}));
-
+// app.use(cors({
+//     origin:'http://localhost:3000',
+//     credentials:true
+// }));
+app.use(cors());
 // app.use(cors());
 app.set("trust proxy", 1);
 app.use(
